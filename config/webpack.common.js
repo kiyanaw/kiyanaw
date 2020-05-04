f166a30786433c5cd6e93732243fb58c45ef9c0a
Aaron Fay <aaron.j.fay@gmail.com>
2020-05-04 09:02:10 -0700

    post-amplify

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,74 +1,79 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-const entryFile = path.join(__dirname + '/../src/main.js');
+const entryFile = path.join(__dirname + "/../src/main.js");
 
-module.exports = function (options) {
+module.exports = function(options) {
   return {
     mode: options.mode,
     entry: entryFile,
 
     resolve: {
-      extensions: ['.js', '.json', '.vue'],
-      modules: [path.join(__dirname, '../src'), 'node_modules'],
+      extensions: [".mjs", ".js", ".json", ".vue"],
+      modules: [path.join(__dirname, "../src"), "node_modules"],
       alias: {
-        'vue$': 'vue/dist/vue.common.js',
-        'src': path.resolve(__dirname, '../src/'),
-        'assets': path.resolve(__dirname, '../src/assets/'),
-        'pages': path.resolve(__dirname, '../src/assets/vue/pages/'),
-        'components': path.resolve(__dirname, '../src/assets/vue/components/')
+        vue$: "vue/dist/vue.common.js",
+        src: path.resolve(__dirname, "../src/"),
+        assets: path.resolve(__dirname, "../src/assets/"),
+        pages: path.resolve(__dirname, "../src/assets/vue/pages/"),
+        components: path.resolve(__dirname, "../src/assets/vue/components/")
       }
     },
 
     output: {
       pathinfo: true,
       devtoolLineToLine: true,
-      filename: '[hash].[name].js',
+      filename: "[hash].[name].js",
       sourceMapFilename: "[hash].[name].js.map",
-      path: path.join(__dirname, '../www')
+      path: path.join(__dirname, "../www")
     },
 
     module: {
-      rules: [{
+      rules: [
+        {
           test: /\.(png|jpe?g|gif)$/,
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: '[name].[ext]?[hash]'
+            name: "[name].[ext]?[hash]"
           }
         },
         {
           test: /\.(woff2?|eot|ttf|otf|mp3|wav)(\?.*)?$/,
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: '[name].[ext]?[hash]'
+            name: "[name].[ext]?[hash]"
           }
         },
         {
           test: /\.svg$/,
-          loader: 'url-loader'
+          loader: "url-loader"
         },
         {
           test: /\.scss$/,
-          loader: ['vue-style-loader', 'css-loader', 'sass-loader']
+          loader: ["vue-style-loader", "css-loader", "sass-loader"]
         },
         {
           test: /\.sass$/,
-          loader: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
+          loader: [
+            "vue-style-loader",
+            "css-loader",
+            "sass-loader?indentedSyntax"
+          ]
         },
         {
           test: /\.vue$/,
           exclude: /node_modules/,
-          loader: 'vue-loader',
+          loader: "vue-loader",
           options: {
             loaders: {
               js: {
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 options: {
-                  presets: ['env'],
-                  plugins: ['transform-object-rest-spread']
+                  presets: ["env"],
+                  plugins: ["transform-object-rest-spread"]
                 }
               }
             }
@@ -78,10 +83,10 @@ module.exports = function (options) {
           test: /\.js$/,
           exclude: /node_modules(\/|\\)(?!(framework7|framework7-vue|template7|dom7)(\/|\\)).*/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['env'],
-              plugins: ['transform-runtime', 'transform-object-rest-spread']
+              presets: ["env"],
+              plugins: ["transform-runtime", "transform-object-rest-spread"]
             }
           }
         }
@@ -90,14 +95,15 @@ module.exports = function (options) {
 
     plugins: [
       new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify(options.mode)
+        "process.env": {
+          NODE_ENV: JSON.stringify(options.mode)
         }
       }),
       new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/index.ejs',
-        platform: process.argv.length > 5 ? process.argv[5].replace(/[- ]/g, '') : "",
+        filename: "index.html",
+        template: "src/index.ejs",
+        platform:
+          process.argv.length > 5 ? process.argv[5].replace(/[- ]/g, "") : "",
         inject: true,
         minify: {
           removeComments: true,
@@ -111,5 +117,5 @@ module.exports = function (options) {
       }),
       new VueLoaderPlugin()
     ]
-  }
-}
+  };
+};
--- src/routes.js
+++ src/routes.js
@@ -1,44 +1,46 @@
-import Home from './assets/vue/pages/home.vue';
-import AboutPage from './assets/vue/pages/about.vue';
-import FormPage from './assets/vue/pages/form.vue';
-import DynamicRoutePage from './assets/vue/pages/dynamic-route.vue';
+import Vue from 'vue'
 
-import PanelLeftPage from './assets/vue/pages/panel-left.vue';
-import ColorThemes from './assets/vue/pages/color-themes.vue';
-import Chat from './assets/vue/pages/chat.vue';
-import Vuex from './assets/vue/pages/vuex.vue';
+import Home from './assets/vue/pages/home.vue'
+import AboutPage from './assets/vue/pages/about.vue'
+import FormPage from './assets/vue/pages/form.vue'
+import DynamicRoutePage from './assets/vue/pages/dynamic-route.vue'
+
+import PanelLeftPage from './assets/vue/pages/panel-left.vue'
+import ColorThemes from './assets/vue/pages/color-themes.vue'
+import Chat from './assets/vue/pages/chat.vue'
+import Vuex from './assets/vue/pages/vuex.vue'
 
 export default [
   {
     path: '/',
-    component: Home
+    component: Home,
   },
   {
     path: '/about/',
-    component: AboutPage
+    component: AboutPage,
   },
   {
     path: '/form/',
-    component: FormPage
+    component: FormPage,
   },
   {
     path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage
+    component: DynamicRoutePage,
   },
   {
     path: '/panel-left/',
-    component: PanelLeftPage
+    component: PanelLeftPage,
   },
   {
     path: '/color-themes/',
-    component: ColorThemes
+    component: ColorThemes,
   },
   {
     path: '/chat/',
-    component: Chat
+    component: Chat,
   },
   {
     path: '/vuex/',
-    component: Vuex
+    component: Vuex,
   },
-];
+]

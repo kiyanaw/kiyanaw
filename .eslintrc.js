e3ce91db98d58d50d1b160ad70381fe0a4ba594d
Jed Palmater <jed@palmater.ca>
2020-05-08 15:49:03 -0300

    got vue eslint stuff working again, customized silly rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
-  "parser": "babel-eslint",
+  "parser":  "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "sourceType": "module"
+  },
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     'airbnb',
     'airbnb/hooks',
-    'plugin:vue/recommended'
+    'plugin:vue/vue3-recommended'
   ],
   rules: {
     'max-len': [1, 120],
     'no-console': 0,
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    "vue/component-tags-order": ["warn", {
+      "order": ["template", "script", "style"]
+    }],
+    "vue/no-deprecated-slot-attribute": 0,
   }
 };

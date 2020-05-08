f553f84eda7358d3ed83a54af49ae36b28bc0afd
Jed Palmater <jed@palmater.ca>
2020-05-08 18:12:01 -0300

    stubbed out regions and dialects

--- src/pages/preferences.vue
+++ src/pages/preferences.vue
@@ -8,7 +8,7 @@
       <h1>
         User Settings
       </h1>
-
+      <!-- Language Setting -->
       <div class="list">
         <ul>
           <li>
@@ -36,6 +36,65 @@
             </a>
           </li>
         </ul>
+
+        <!-- Dialect -->
+        <div class="list">
+          <ul>
+            <li>
+              <a
+                href="#"
+                class="item-link smart-select"
+                data-close-on-select="true">
+                <select
+                  v-model="userDialect"
+                  name="dialect">
+                  <option
+                    v-for="dialect in dialects"
+                    :key="dialect"
+                    :value="dialect"
+                    :selected="userDialect === dialect">
+                    {{ dialect }}
+                  </option>
+                </select>
+                <div class="item-content">
+                  <div class="item-inner">
+                    <div class="item-title"> Dialect </div>
+                    <div class="item-after">{{ userDialect }}</div>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="list">
+          <ul>
+            <li>
+              <a
+                href="#"
+                class="item-link smart-select"
+                data-close-on-select="true">
+                <select
+                  v-model="userRegion"
+                  name="region">
+                  <option
+                    v-for="region in regions"
+                    :key="region"
+                    :value="region"
+                    :selected="userRegion === region">
+                    {{ region }}
+                  </option>
+                </select>
+                <div class="item-content">
+                  <div class="item-inner">
+                    <div class="item-title"> Region </div>
+                    <div class="item-after">{{ userRegion }}</div>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </f7-block>
   </f7-page>
@@ -48,6 +107,8 @@ export default {
   name: 'Preferences',
   data: () => ({
     languages: ['Cree', "Mi'kmaq"],
+    dialects: ['test1', 'test2', 'test3', 'test4'],
+    regions: ['r1', 'r2', 'r3', 'r4'],
   }),
   computed: {
     ...mapGetters([
@@ -59,6 +120,22 @@ export default {
       },
       set(val) {
         this.updateAttribute({ name: 'language', value: val })
+      },
+    },
+    userDialect: {
+      get() {
+        return this.user.dialect
+      },
+      set(val) {
+        this.updateAttribute({ name: 'dialect', value: val })
+      },
+    },
+    userRegion: {
+      get() {
+        return this.user.region
+      },
+      set(val) {
+        this.updateAttribute({ name: 'region', value: val })
       },
     },
   },

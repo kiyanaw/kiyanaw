a62fd095c05e8b3de7662706f8585a8141a59bc2
Aaron Fay <aaron.j.fay@gmail.com>
2020-05-19 20:34:55 -0700

    formatting tweaks

--- config/webpack.release.js
+++ config/webpack.release.js
@@ -1,32 +1,32 @@
-const path = require('path');
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.common');
+const path = require('path')
+const webpackMerge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const commonConfig = require('./webpack.common')
 
-const CordovaHtmlOutputPlugin = require('../webpack/plugins/CordovaHtmlOutputPlugin.js');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CordovaHtmlOutputPlugin = require('../webpack/plugins/CordovaHtmlOutputPlugin.js')
 
-module.exports = function() {
+module.exports = function () {
   return webpackMerge(commonConfig({ mode: 'production' }), {
     module: {
       rules: [
         {
           test: /\.css$/,
-          loader: ['style-loader', 'css-loader']
-        }
-      ]
+          loader: ['style-loader', 'css-loader'],
+        },
+      ],
     },
     plugins: [
       new CordovaHtmlOutputPlugin(),
       new TerserPlugin({
-        extractComments: false
+        extractComments: false,
       }),
       new CleanWebpackPlugin({
         dry: false,
         verbose: false,
         cleanOnceBeforeBuildPatterns: ['!index.html'],
-        cleanAfterEveryBuildPatterns: ['!index.html']
-      })
-    ]
-  });
-};
+        cleanAfterEveryBuildPatterns: ['!index.html'],
+      }),
+    ],
+  })
+}

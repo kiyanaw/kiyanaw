8edf34a3887953eb728321422d9fc80be32e1a89
Aaron Fay <aaron.j.fay@gmail.com>
2020-05-20 20:57:49 -0700

    catch error in service client

--- src/services/client.js
+++ src/services/client.js
@@ -4,12 +4,18 @@ import { Auth } from '@aws-amplify/auth'
 export default {
   async request(query, variables) {
     // NOTE: we intentionally go around the store here
-    const user = await Auth.currentAuthenticatedUser({ bypassCache: false })
+    let user = null
     let authMode = 'AMAZON_COGNITO_USER_POOLS'
+    try {
+      user = await Auth.currentAuthenticatedUser({ bypassCache: false })
+    } catch (error) {
+      // no user
+    }
     if (!user) {
       // AWS_IAM will allow for public: read as per the graphql schema
       authMode = 'AWS_IAM'
     }
+    console.log('Client query authMode', authMode)
 
     return API.graphql({ query, variables, authMode })
   },
